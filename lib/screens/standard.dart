b890d3cf0646a60ef7c4f8044202db0c3fafa917
Al-Hussein <tvclockdev@gmail.com>
2021-11-22 21:56:17 +0200

    1 - map with current location work
    next step:
    1 - working on request permission and service

--- lib/screens/standard.dart
+++ lib/screens/standard.dart
@@ -1,4 +1,5 @@
 import 'dart:async';
+import 'package:location/location.dart';
 
 import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
@@ -18,15 +19,77 @@ class StandardMapPage extends StatefulWidget {
 class _StandardMapPageState extends State<StandardMapPage> {
   final Completer<GoogleMapController> _mapController = Completer();
 
+  static final CameraPosition _kGooglePlex = CameraPosition(
+      target: LatLng(37.42796133580664, -122.085749655962), zoom: 14.4746);
+
+  Location location = Location();
+
+  bool? _serviceEnabled;
+  PermissionStatus? _permissionGranted;
+
+
+  Future<void> _checkService() async {
+    final bool serviceEnabledResult = await location.serviceEnabled();
+    setState(() {
+      _serviceEnabled = serviceEnabledResult;
+    });
+  }
+
+  Future<void> _requestService() async {
+    if (_serviceEnabled == true) {
+      return;
+    }
+    final bool serviceRequestedResult = await location.requestService();
+    setState(() {
+      _serviceEnabled = serviceRequestedResult;
+    });
+  }
+
+  Future<void> _checkPermissions() async {
+    final PermissionStatus permissionGrantedResult =
+    await location.hasPermission();
+    setState(() {
+      _permissionGranted = permissionGrantedResult;
+    });
+  }
+
+  Future<void> _requestPermission() async {
+    if (_permissionGranted != PermissionStatus.granted) {
+      final PermissionStatus permissionRequestedResult =
+      await location.requestPermission();
+      setState(() {
+        _permissionGranted = permissionRequestedResult;
+      });
+    }
+  }
+
+
   @override
   void initState() {
     super.initState();
+
+    _mapController.future.whenComplete(() => {
+      // _checkPermissions();
+      // _requestPermission();
+      // _checkService();
+      // _requestService();
+    });
   }
 
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
     return Scaffold(
+      body: Container(
+        height: MediaQuery.of(context).size.height,
+        width: MediaQuery.of(context).size.width,
+        child: GoogleMap(
+          mapType: MapType.normal,
+          initialCameraPosition: _kGooglePlex,
+          onMapCreated: (controller) => _mapController.complete(controller),
+          myLocationEnabled: true,
+        ),
+      ),
       appBar: AppBar(
         title: Text(widget.title),
       ),
